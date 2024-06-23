d2ab18d49b45a05343d89fcfb19137f4c2b52b70
Hugo Messias <hugomessias9@gmail.com>
2024-06-22 21:11:42 -0300

    reorganized and changed the position of some methods
    moved read_file() from BufferManager to DataProperties
    
    turned BufferManager.queue_manager() into __queue_manager() and start_queue_manager(): the first contains the logic and the second calls the first on a new thread
    
    renamed _play() to play() and the previous play() to play_and_wait()
    
    renamed append_to_queue() to enqueue()
    
    safe_append() now can alternate between just waiting with sleep and waiting while checking if should be interrupted

--- BufferedAudio.py
+++ BufferedAudio.py
@@ -14,6 +14,13 @@ class DataProperties():
         self.samplerate = info.samplerate
         self.size = len(data[0])
         self.duration = info.duration
+    
+    @staticmethod
+    def read_file(file_path: str):
+        """Reads the file and organizes its contents into a DataProperties object."""
+        data = sf.read(file_path)
+        info = sf.info(file_path)
+        return DataProperties(data, info)
 
 
 class BufferManager():
@@ -39,17 +46,26 @@ class BufferManager():
         self.buffer_lock = threading.Lock()
 
         self.volume = volume
-        self._volume_scale = 10**(volume/20)
+        self.volume_scale = 10**(volume/20)
 
         self.files_queue = []
         self.clear_queue_flag = False
+    
+    def trim(self, wait=0.05):
+        """Clears the already read portion of the buffer.
+        \nUses a comparison between self.playing_time and self.last_written_byte to decide what to trim."""
+        with self.buffer_lock:
+            last_read_byte = int((self.playing_time)*self.samplerate)
+            time.sleep(wait)
+            if last_read_byte < 0:
+                return
 
-    @staticmethod
-    def read_file(file_path: str):
-        """Reads the file and organizes its contents into a DataProperties object."""
-        data = sf.read(file_path)
-        info = sf.info(file_path)
-        return DataProperties(data, info)
+            if self.last_written_byte > last_read_byte:
+                self.buffer[self.last_written_byte:] = 0
+                self.buffer[:last_read_byte] = 0
+
+            elif self.last_written_byte < last_read_byte:
+                self.buffer[self.last_written_byte:last_read_byte] = 0
     
     def append(self, data: DataProperties):
         """Appends an audio file to the end of the buffer. Also overflows back to the beggining of the buffer when the end is reached."""
@@ -71,78 +87,21 @@ class BufferManager():
                 size_of_second_slice = data.size - bytes_before_overflow
 
                 # write the pre-overflow and then the overflowed portion of the audio file
-                self.buffer[self.last_written_byte:] = data.data[:size_of_first_slice] * self._volume_scale
-                self.buffer[0:size_of_second_slice] = data.data[size_of_first_slice:] * self._volume_scale
+                self.buffer[self.last_written_byte:] = data.data[:size_of_first_slice] * self.volume_scale
+                self.buffer[0:size_of_second_slice] = data.data[size_of_first_slice:] * self.volume_scale
 
                 # update last_used_byte
                 self.last_written_byte = size_of_second_slice
 
             else:
                 # just write the audio file to the buffer and then update last_used_byte
-                self.buffer[self.last_written_byte:new_last_used_byte] = data.data * self._volume_scale
+                self.buffer[self.last_written_byte:new_last_used_byte] = data.data * self.volume_scale
                 self.last_written_byte = new_last_used_byte
-
-    def force_now(self, file_path: str):
-        """Forces an audio file to be played immediatelly and sets the last used byte to after that file, essencially clearing the rest of the buffer."""
-        self.files_queue.clear()
-        self.clear_queue_flag = True
-        self.last_written_byte = int((self.playing_time)*self.samplerate)
-        data = self.read_file(file_path)
-        self.append(data)
-        self.trim()
-          
-    # TODO: force_and_keep(): function to force an audio but keep the buffer the way it was, with the same last_used_byte as before
-        # last_used_byte could be the previous value (if grater tha the newer one), or the new one (which is the default on self.append)
-
-    def trim(self, wait=0.05):
-        """Clears the already read portion of the buffer.
-        \nUses a comparison between self.playing_time and self.last_written_byte to decide what to trim."""
-        with self.buffer_lock:
-            last_read_byte = int((self.playing_time)*self.samplerate)
-            time.sleep(wait)
-            if last_read_byte < 0:
-                return
-
-            if self.last_written_byte > last_read_byte:
-                self.buffer[self.last_written_byte:] = 0
-                self.buffer[:last_read_byte] = 0
-
-            elif self.last_written_byte < last_read_byte:
-                self.buffer[self.last_written_byte:last_read_byte] = 0
-
-    def _time_tracker(self):
-        """Intended for use only inside of the _play() method. 
-        \nConstantly updates self.playing_time to keep up with the bytes being played at the momment.
-        \nCurrently works separate from the playing thread, which could maybe cause desynchronization."""
-        while True:
-            self.playing_time = 0
-            start_timer = time.perf_counter()
-            while self.playing_time <= self.buffer_time:
-                curr_timer = time.perf_counter()
-                self.playing_time = curr_timer - start_timer
-                time.sleep(1/1000)
-
-    def play(self):
-        """Plays the audio buffer. 
-        \nAlso waits on a separate thread for something to be written to the buffer before starting to play."""
-        # calls the _play() functiion and make it wait on a separate thread
-        play = threading.Thread(target=self._play, args=(), daemon=True)
-        play.start()
-
-    def _play(self):
-        """Intended for use only inside of the play() method."""
-        # wait for something to be written to the buffer before starting to play
-        while np.all(self.buffer == 0):
-            pass
-        tracker = threading.Thread(target=self._time_tracker, args=(), daemon=True)
-        
-        sd.play(self.buffer, self.samplerate, loop=True)
-        tracker.start()
-
-    def safe_append(self, file_path: str):
+    
+    def safe_append(self, file_path: str, wait_type: typing.Literal["sleep", "queue"] = "sleep"):
         """Waits for a large enough chunk of the buffer to be trimmed before appending the data."""
         # get data and check if it fits the buffer
-        data = self.read_file(file_path)
+        data = DataProperties.read_file(file_path)
         if data.size > self.buffer_size:
             message = f"the size of the file ({round(data.size/1000000/2*1.048576, 2)}mb) is bigger than the size of the buffer ({round(self.buffer_size/1000000/2*1.048576, 2)}mb)"
             raise MemoryError(message)
@@ -170,48 +129,50 @@ class BufferManager():
         print("missing bytes #1:",missing_bytes,"| data.size:",data.size,"| time needed:",time_needed)
 
         # wait for those bytes to be read and then trim the buffer
-        # TODO: reuse the previous implementation when the function is not called from queue_manager
-        # time.sleep(time_needed)
-        time_needed += self.playing_time
-        if overflow:
-            curr_playing_time = self.playing_time
-            while curr_playing_time <= self.playing_time < self.buffer_time:
+        if wait_type == "sleep":
+            time.sleep(time_needed)
+
+        elif wait_type == "queue":
+            time_needed += self.playing_time
+            if overflow:
+                curr_playing_time = self.playing_time
+                while curr_playing_time <= self.playing_time < self.buffer_time:
+                    if self.clear_queue_flag:
+                        print("THREAD INTERRUPTED")
+                        self.clear_queue_flag = False
+                        return
+                    time.sleep(1/40)
+                time_needed -= self.buffer_time
+            while self.playing_time < time_needed:
                 if self.clear_queue_flag:
                     print("THREAD INTERRUPTED")
                     self.clear_queue_flag = False
                     return
                 time.sleep(1/40)
-            time_needed -= self.buffer_time
-        while self.playing_time < time_needed:
-            if self.clear_queue_flag:
-                print("THREAD INTERRUPTED")
-                self.clear_queue_flag = False
-                return
-            time.sleep(1/40)
-        self.trim()
+            self.trim()
+            
+        else:
+            message = f"wait_type expected to be 'sleep' or 'queue', got '{wait_type}' instead"
+            raise AttributeError(message)
 
         # append the file on the cleared space
         self.append(data)
         print("APPENDED","| name:",file_path,"| first_byte:",first_byte,"| final_byte:",final_byte,"| duration:",data.duration)
+
+    def insert_at_playhead(self, file_path: str):
+        """Forces an audio file to be played immediatelly and sets the last used byte to after that file, essencially clearing the rest of the buffer."""
+        self.files_queue.clear()
+        self.clear_queue_flag = True
+        self.last_written_byte = int((self.playing_time)*self.samplerate)
+        data = DataProperties.read_file(file_path)
+        self.append(data)
+        self.trim()
+          
+    # TODO: force_and_keep(): function to force an audio but keep the buffer the way it was, with the same last_used_byte as before
+        # last_used_byte could be the previous value (if grater tha the newer one), or the new one (which is the default on self.append)
     
-    # TODO: organize it into a new queue sub class
-        # remember to start the queue thread inside it
-    def queue_manager(self):
-        """Appends the files from the queue when apropriate. Must be called on a separated thread."""
-        while True:
-            if self.files_queue:
-                file_name = self.files_queue.pop(0)
-                print(f"APPENDING: {file_name}")
-                self.safe_append(file_name)
-            time.sleep(1/40)
-    
-    def append_to_queue(self, file_name):
-        """Adds elements to the queue."""
-        if not os.path.exists(file_name):
-            message = f"the file '{file_name}' does not exist"
-            raise ValueError(message)
-        self.files_queue.append(file_name)              
-    
+    # TODO: force_and_continue(): inserts the file at the pointer and offsets what was already there to the end of the inserted file
+
     def change_volume(self, volume: int):
         """Changes the loudness of the audio by n decibels.
         \nPositive values will increase the volume by n decibels.
@@ -220,35 +181,85 @@ class BufferManager():
         if volume:
             self.volume += volume
             self.buffer[:] *= 10**(volume/20)
-            self._volume_scale = 10**(self.volume/20)
+            self.volume_scale = 10**(self.volume/20)
         else:
             self.volume = 0
-            self.buffer[:] /= self._volume_scale
-            self._volume_scale = 10**(volume/20)
+            self.buffer[:] /= self.volume_scale
+            self.volume_scale = 10**(volume/20)
+
+    def __time_tracker(self):
+        """Intended for use only inside of the _play() method. 
+        \nConstantly updates self.playing_time to keep up with the bytes being played at the momment.
+        \nCurrently works separate from the playing thread, which could maybe cause desynchronization."""
+        print("CALLED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        while True:
+            self.playing_time = 0
+            start_timer = time.perf_counter()
+            while self.playing_time <= self.buffer_time:
+                curr_timer = time.perf_counter()
+                self.playing_time = curr_timer - start_timer
+                time.sleep(1/1000)
+
+    def play(self):
+        """Plays the audio from the buffer."""
+        # wait for something to be written to the buffer before starting to play
+        while np.all(self.buffer == 0):
+            pass
+        tracker = threading.Thread(target=self.__time_tracker, args=(), daemon=True)
+        
+        sd.play(self.buffer, self.samplerate, loop=True)
+        tracker.start()
     
+    def wait_and_play(self):
+        """Waits on a separate thread for something to be written to the buffer before starting to play."""
+        # calls the _play() functiion and make it wait on a separate thread
+        play = threading.Thread(target=self.play, args=(), daemon=True)
+        play.start()
+    
+    def __queue_manager(self):
+        """Should be called only inside of the play() method.
+        \nAppends the files from the queue when apropriate."""
+        while True:
+            if self.files_queue:
+                file_name = self.files_queue.pop(0)
+                print(f"APPENDING: {file_name}")
+                self.safe_append(file_name, "queue")
+            time.sleep(1/40)
+    
+    def start_queue_manager(self):
+        """Starts the queue_manager thread.
+        \nAppends the files from the queue when apropriate."""
+        queue_manager = threading.Thread(target=self.__queue_manager, args=(), daemon=True)
+        queue_manager.start()
+    
+    def enqueue(self, file_name: str):
+        """Adds elements to the queue."""
+        if not os.path.exists(file_name):
+            message = f"the file '{file_name}' does not exist"
+            raise ValueError(message)
+        self.files_queue.append(file_name)            
 
 
 
 if __name__ == "__main__":
     bf = BufferManager(4, file_sample="ignore/Track_096.ogg", volume=-5)
 
-    bf.play()
-    queue = threading.Thread(target=bf.queue_manager, args=(), daemon=True)
-    queue.start()
+    bf.wait_and_play()
+    bf.start_queue_manager()
     with open("ignore/GTA SA Radio.m3u8", 'r') as playlist:
         for line in playlist:
             path = "C:\\VSCode\\JavaScript\\GTASARADIO\\audio\\"
             # print(line)
             line = path + line[line.find("STREAMS")+7:].replace('.mp3', '.ogg').rstrip()
             # print(line)
-            bf.append_to_queue(line)
+            bf.enqueue(line)
             # input()
     input()
     print(bf.files_queue)
     input()
-    bf.force_now("ignore/Track_040.ogg")
+    bf.insert_at_playhead("ignore/Track_040.ogg")
     print(bf.files_queue)
     # time.sleep(10)
     input()
-    bf.append_to_queue("ignore/Track_099.ogg")
+    bf.enqueue("ignore/Track_099.ogg")
     input()
