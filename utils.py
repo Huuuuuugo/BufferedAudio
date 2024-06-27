95e4c422c66fdbd1d59557137b26eeab282287b6
Hugo Messias <hugomessias9@gmail.com>
2024-06-27 20:57:53 -0300

    changed CriticalThread.check_exceptions() to CriticalThread.wait_exception(): the function now receives a timeout argument and keeps checking until that timeout is reached or an exception is raised, a timeout of None checks it untill something is found and 0 checks it only once; the class also no longer uses flags, and can be interrupted with Ctrl+C; the class now also prints a warning to stderr when an exception is raised on a thread and includes the name of the thread where it came from when it's re-raised on the main thread.

--- utils.py
+++ utils.py
@@ -1,21 +1,46 @@
 import threading
+import time
+import sys
+
 
 class CriticalThread(threading.Thread):
     """Child class of threading.Thread that acts mostly the same way, but also re-raises the last exception raised by any CriticalThread on the main thread.
     \nIn order for it to work, CriticalThread.check_exceptions() must be called periodically on the main thread."""
-    exception_flag = threading.Event()
-    exception = None
-
+    exception_info = {}
+    
     @classmethod
-    def check_exceptions(cls):
-        """When CriticalThread.check_exceptions() is called on the main thread, it checks if any exception was raised on any active CriticalThread, if so, this exception is raised again on the main thread, finishing the program if not handled."""
-        if cls.exception_flag.wait(0.0001):
-            raise cls.exception
+    def wait_exception(cls, timeout: float=None):
+        """When CriticalThread.check_exceptions() is called on the main thread, it waits for any exception to be raised on any active CriticalThread, if so, this exception is raised again on the main thread, finishing the program if not handled."""
+        if timeout is not None:
+            curr_time = 0
+            end_time = time.perf_counter() + timeout
+        
+            while curr_time <= end_time:
+                curr_time = time.perf_counter()
+                if cls.exception_info:
+                    original_exception = cls.exception_info["exception"]
+                    if original_exception.args:
+                        new_message = f"Exception raised on {cls.exception_info["thread_name"]}: {original_exception.args[0]}"
+                    else:
+                        new_message = f"Exception raised on {cls.exception_info["thread_name"]}."
+                    raise type(original_exception)(new_message).with_traceback(original_exception.__traceback__)
+                time.sleep(1/1000)
+                
+        else:
+            while True:
+                if cls.exception_info:
+                    original_exception = cls.exception_info["exception"]
+                    if original_exception.args:
+                        new_message = f"Exception raised on {cls.exception_info["thread_name"]}: {original_exception.args[0]}"
+                    else:
+                        new_message = f"Exception raised on {cls.exception_info["thread_name"]}."
+                    raise type(original_exception)(new_message).with_traceback(original_exception.__traceback__)
+                time.sleep(1/40)
     
     def run(self):
         try:
             super(CriticalThread, self).run()
 
         except Exception as e:
-            CriticalThread.exception_flag.set()
-            CriticalThread.exception = e
+            self.__class__.exception_info.update({"thread_name": self.name, "exception": e})
+            print(f"Warning: '{type(e).__name__}' exception raised on {self.name}.", file=sys.stderr)
