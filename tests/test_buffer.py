f9e7c85929b46fdb93e11f6d9ea566f11ca8bc8d
Hugo Messias <hugomessias9@gmail.com>
2024-07-29 12:56:49 -0300

    Improved the test for playing_time accuracy.

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -9,10 +9,24 @@ def test_playing_time_accuracy():
 
     This test starts playback, waits for a known duration, and then checks that the
     playing_time property is close to the expected elapsed time within a small margin of error.
+    This check is repeated for a total of 3 cycles and 5 times per cycle.
+
+    An error here could indicate a problem with: 
+        - `BufferManager.play()`; 
+        - `BufferManager.stop()`;
+        - too much consecutive accesses to time.perf_counter();
+        - the playing_time property itself;
     """
 
     buffer = BufferManager(0.5, 32000)
-    buffer.play()
-    time.sleep(0.5)
-    assert round(buffer.playing_time, 3) in [0.500, 0.501]
-    buffer.stop()
+    for _ in range(3):
+        time.sleep(0.2)  # Small delay before playing to see if it will affect the time after
+        buffer.play()
+
+        for i in range(1, 6):
+            time.sleep(0.1)  # Simulate playback for 0.1 seconds
+            actual_time = round(buffer.playing_time, 2)
+            expected_time = round(0.1*i, 2)
+            assert actual_time == expected_time, f"Inaccurate result: {expected_time = } | {actual_time = }"
+
+        buffer.stop()
